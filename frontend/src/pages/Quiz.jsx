6595ffd2a02b95f04697fdf2d135889691771c5b
IrnehBrgt <henri.bergeret@gmail.com>
2022-04-20 15:20:05 +0200

    modify the quiz

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -5,53 +5,52 @@ import { NavLink } from "react-router-dom";
 function Quiz() {
   const questions = [
     {
+      questionText: "Etes vous un homme ou une femme ?",
+      answerOptions: [{ answerText: "Homme" }, { answerText: "Femme" }],
+    },
+    {
       questionText: "Dans quelle tranche d'âge vous situez vous ?",
       answerOptions: [
-        { answerText: "0-20 ans ", score: 4 },
-        { answerText: "20-40 ans", score: 3 },
-        { answerText: "40-60 ans", score: 2 },
-        { answerText: "60 ans ou plus", score: 1 },
+        { answerText: "0-20 ans " },
+        { answerText: "20-40 ans" },
+        { answerText: "40-60 ans" },
+        { answerText: "60 ans ou plus" },
       ],
     },
     {
       questionText: "Quel est votre niveau de pratique sportive ?",
       answerOptions: [
-        { answerText: "Plus de 3 fois par semaine", score: 4 },
-        { answerText: "Entre 1 et 3 fois par semaine", score: 3 },
-        { answerText: "Moins d'une fois par semaine", score: 2 },
-        { answerText: "Moins d'une fois par mois", score: 1 },
+        { answerText: "Plus de 3 fois par semaine" },
+        { answerText: "Entre 1 et 3 fois par semaine" },
+        { answerText: "Moins d'une fois par semaine" },
+        { answerText: "Moins d'une fois par mois" },
       ],
     },
     {
       questionText: "Vous êtes plutôt sport collectif ou sport individuel ?",
       answerOptions: [
-        { answerText: "Sport collectif", score: 4 },
-        { answerText: "Sport individuel", score: 3 },
-        { answerText: "Autant l'un que l'autre", score: 2 },
-        { answerText: "Je n'aime pas le sport", score: 1 },
+        { answerText: "Sport collectif" },
+        { answerText: "Sport individuel" },
+        { answerText: "Autant l'un que l'autre" },
+        { answerText: "Je n'aime pas le sport" },
       ],
     },
     {
-      questionText: "Quelle type d'activité vous plaît le plus ?",
-      answerOptions: [
-        { answerText: "Activité de glisse ", score: 4 },
-        { answerText: "Sport de balles ", score: 3 },
-        { answerText: "Activité en salle", score: 2 },
-        { answerText: "Activité contemplative", score: 1 },
-      ],
+      questionText: "Aimez vous la ride ?",
+      answerOptions: [{ answerText: "Oui" }, { answerText: "Non" }],
     },
   ];
-  const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
   // eslint-disable-next-line no-unused-vars
   const handleAnswerButtonClick = (answerOption) => {
     const nextQuestion = currentQuestion + 1;
     setCurrentQuestion(nextQuestion);
-    setScore(questions[currentQuestion].answerOptions.score);
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
-      setCurrentQuestion(3);
+      setCurrentQuestion(4);
+      setShowResult(true);
     }
   };
 
@@ -65,13 +64,17 @@ function Quiz() {
           </button>
         ))}
       </div>
-      <div className="show-score">Votre score est : {score}</div>
-
-      <button type="button" className="quiz-result-button">
-        <NavLink className="quiz-result-link" to="/QuizResult">
-          Voir les activités proposées
-        </NavLink>
-      </button>
+      <div className="show-result-button">
+        {showResult ? (
+          <button type="button" className="quiz-result-button">
+            <NavLink className="quiz-result-link" to="/QuizResult">
+              Voir les activités proposées
+            </NavLink>
+          </button>
+        ) : (
+          <p />
+        )}
+      </div>
     </div>
   );
 }
