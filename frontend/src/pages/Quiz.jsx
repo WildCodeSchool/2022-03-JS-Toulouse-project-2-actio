0104189d7c76087b0a35160b944adc73bfcf615e
IrnehBrgt <henri.bergeret@gmail.com>
2022-04-20 16:39:45 +0200

    add an index in the question to decide which will be the next

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -5,10 +5,12 @@ import { NavLink } from "react-router-dom";
 function Quiz() {
   const questions = [
     {
+      id: 1,
       questionText: "Etes vous un homme ou une femme ?",
       answerOptions: [{ answerText: "Homme" }, { answerText: "Femme" }],
     },
     {
+      id: 2,
       questionText: "Dans quelle tranche d'âge vous situez vous ?",
       answerOptions: [
         { answerText: "0-20 ans " },
@@ -18,6 +20,7 @@ function Quiz() {
       ],
     },
     {
+      id: 3,
       questionText: "Quel est votre niveau de pratique sportive ?",
       answerOptions: [
         { answerText: "Plus de 3 fois par semaine" },
@@ -27,6 +30,7 @@ function Quiz() {
       ],
     },
     {
+      id: 4,
       questionText: "Vous êtes plutôt sport collectif ou sport individuel ?",
       answerOptions: [
         { answerText: "Sport collectif" },
@@ -36,16 +40,19 @@ function Quiz() {
       ],
     },
     {
+      id: 5,
       questionText: "Aimez vous la ride ?",
       answerOptions: [{ answerText: "Oui" }, { answerText: "Non" }],
     },
   ];
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showResult, setShowResult] = useState(false);
+  const [questionId, setQuestionId] = useState(questions[0].id);
   // eslint-disable-next-line no-unused-vars
   const handleAnswerButtonClick = (answerOption) => {
     const nextQuestion = currentQuestion + 1;
-    setCurrentQuestion(nextQuestion);
+    setCurrentQuestion(currentQuestion + 1);
+    setQuestionId(questions[nextQuestion].id);
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
@@ -63,6 +70,9 @@ function Quiz() {
             {answerOption.answerText}
           </button>
         ))}
+      </div>
+      <div>
+        <p>{questionId}</p>
       </div>
       <div className="show-result-button">
         {showResult ? (
