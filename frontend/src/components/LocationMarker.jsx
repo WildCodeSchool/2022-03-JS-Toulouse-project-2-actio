b3e823eabfd837df49480ee19181eb9a56f6633b
shifumy <ymblandin@gmail.com>
2022-04-20 15:01:57 +0200

    code reorganization

--- frontend/src/components/LocationMarker.jsx
+++ frontend/src/components/LocationMarker.jsx
@@ -3,12 +3,13 @@ import { useMap, Marker } from "react-leaflet";
 
 function LocationMarker() {
   const [position, setPosition] = useState(null);
-
   const map = useMap();
+
   const handleOnLocationFound = (e) => {
     setPosition(e.latlng);
     map.flyTo(e.latlng, map.getZoom());
   };
+
   useEffect(() => {
     map.locate().on("locationfound", handleOnLocationFound);
   }, [map]);
