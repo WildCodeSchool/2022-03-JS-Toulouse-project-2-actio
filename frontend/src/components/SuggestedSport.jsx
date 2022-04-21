f72be47a46d0627d029bfd07a2da29113b8c59b3
shifumy <ymblandin@gmail.com>
2022-04-21 16:56:58 +0200

    create the component SuggestedSport and style it

---
+++ frontend/src/components/SuggestedSport.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SuggestedSport.css";
+
+function SuggestedSport() {
+  return (
+    <div className="SuggestedSport">
+      <img src="/src/assets/icons8-football-100.png" alt="icone foot" />
+      <p>Football</p>
+    </div>
+  );
+}
+
+export default SuggestedSport;
