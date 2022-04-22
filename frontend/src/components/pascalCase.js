e448851979f7dddaaf21b206f30f041183ae1c1c
shifumy <ymblandin@gmail.com>
2022-04-22 15:23:12 +0200

    refactor some code

--- frontend/src/components/pascalCase.js
+++ frontend/src/components/pascalCase.js
@@ -1,8 +1,8 @@
-function PascalCase(str) {
+function pascalCase(str) {
   return str.replace(
     /\w\S*/g,
     (m) => m.charAt(0).toUpperCase() + m.substr(1).toLowerCase()
   );
 }
 
-export default PascalCase;
+export default pascalCase;
