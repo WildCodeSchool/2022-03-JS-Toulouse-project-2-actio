6e663e5b65bb621bf960f14ba5a36d8d4e36958c
IrnehBrgt <henri.bergeret@gmail.com>
2022-04-27 17:30:48 +0200

    ajout des options possible de réponse ainsi que les sports correspondant

--- frontend/src/components/reponseOfQuiz.js
+++ frontend/src/components/reponseOfQuiz.js
@@ -1,5 +1,136 @@
+let result = 0;
+
 function reponseOfQuiz(tableOfResult) {
-  return tableOfResult;
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["fitness", "patinage", "natation"];
+  }
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["rugby", "handball", "football"];
+  }
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["patinage", "skate"];
+  }
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["volley-ball", "petanque", "tennis"];
+  }
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["skate", "patinage", "natation", "fitness"];
+  }
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["fitness", "natation"];
+  }
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["skate", "patinage", "natation"];
+  }
+  if (
+    tableOfResult[0] === "15-50 ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["fitness", "natation"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["petanque", "tennis", "patinage"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["petanque", "tennis", "volley-ball"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["tennis", "petanque", "patinage"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport collectif" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["tennis", "petanque"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["fitness", "patinage", "natation"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Oui" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["fitness", "patinage", "natation"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Oui"
+  ) {
+    result = ["fitness", "patinage", "natation"];
+  }
+  if (
+    tableOfResult[0] === "50+ ans" &&
+    tableOfResult[1] === "Sport individuel" &&
+    tableOfResult[2] === "Non" &&
+    tableOfResult[3] === "Non"
+  ) {
+    result = ["fitness", "patinage", "natation"];
+  }
+
+  return result;
 }
 
 export default reponseOfQuiz;
--- frontend/src/pages/QuizResult.jsx
+++ frontend/src/pages/QuizResult.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./QuizResult.css";
-import reponseOfQuiz from "../components/reponseOfQuiz";
 
-function QuizResult() {
+function QuizResult(result) {
   return (
     <div className="quiz-result">
       <h2>
         Les resultats sont tombés, voici les activités que nous vous proposons :
       </h2>
-      <p>{reponseOfQuiz}</p>
+      <div>{result}</div>
     </div>
   );
 }
