5719666e0876686f3f58d3ffa8a505dd598ccd9e
shifumy <ymblandin@gmail.com>
2022-04-28 11:39:24 +0200

    create a getInfos.js file to have a better readability op the component Map.jsx

--- frontend/src/components/FilterMenu.jsx
+++ frontend/src/components/FilterMenu.jsx
@@ -5,17 +5,20 @@ import "./FilterMenu.css";
 function FilterMenu(props) {
   const { sportsSelected, setSportsSelected } = props;
 
-  // const handleChange = (e) => {
-  //   setSportsSelected(e.target.value);
-  // };
-
   const handleChange = (e) => {
-    setSportsSelected([...sportsSelected, e.target.value]);
+    if (sportsSelected.includes(e.target.value)) {
+      const index = sportsSelected.indexOf(e.target.value);
+      sportsSelected.splice(index, 1);
+      setSportsSelected([...sportsSelected]);
+    } else {
+      setSportsSelected([...sportsSelected, e.target.value]);
+    }
   };
 
   return (
     <div className="FilterMenu">
       {/* Add a sort in order to sort the sports alphabetically */}
+      {/* if a sport is passed as an url param, checked the checkbox corresponding to the sport with checked = {sportsSelected.includes(sport.value)} */}
       {sports
         .sort((a, b) => a.label.localeCompare(b.label))
         .map((sport) => (
@@ -26,25 +29,11 @@ function FilterMenu(props) {
               id={sport.value}
               value={sport.value}
               onChange={handleChange}
+              checked={sportsSelected.includes(sport.value)}
             />
             {sport.label}
           </label>
         ))}
-      {/* <select
-        className="sports-select"
-        onChange={handleChange}
-        value={sportsSelected}
-      >
-        <option value="">-- Choisis ton activité --</option> */}
-      {/* Add a sort in order to sort the sports alphabetically */}
-      {/* {sports
-          .sort((a, b) => a.label.localeCompare(b.label))
-          .map((sport) => (
-            <option key={sport.value} value={sport.value}>
-              {sport.label}
-            </option>
-          ))}
-      </select> */}
     </div>
   );
 }
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import axios from "axios";
 import LocationMarker from "../components/LocationMarker";
 import SwitchMapListFilter from "../components/SwitchMapListFilter";
 import FilterMenu from "../components/FilterMenu";
-import pascalCase from "../components/pascalCase";
 import distance from "../components/distance";
+import getInfos from "../components/getInfos";
 import Icon from "../components/Icon";
 import "./Map.css";
 
 function Map() {
   // Set a radius to display markers within this radius in kilometers
-  const radius = 5;
+  const radius = 50;
 
   // sportsSelected is to know which sport has been selected by the user using the filter
   const [sportsSelected, setSportsSelected] = useState([]);
@@ -32,165 +31,8 @@ function Map() {
     }
   }, []);
 
-  const getLocation = () => {
-    switch (sportsSelected) {
-      case "tennis":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=courts-de-tennis&q=&rows=50"
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records.map((el) => ({
-                name: `Court de tennis ${el.fields.index}`,
-                coord: el.fields.geo_point_2d,
-                key: el.recordid,
-              }))
-            )
-          );
-        break;
-      case "patinage":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=patinoires&q="
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records.map((el) => ({
-                name: `${pascalCase(el.fields.nom_complet)} | ${
-                  el.fields.telephone
-                } | ${el.fields.adresse}`,
-                coord: el.fields.geo_point_2d,
-                key: el.recordid,
-              }))
-            )
-          );
-        break;
-      case "skate":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=skate-parcs&q="
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records.map((el) => ({
-                name: `Skatepark ${el.fields.index}`,
-                coord: el.fields.geo_point_2d,
-                key: el.recordid,
-              }))
-            )
-          );
-        break;
-      case "natation":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=piscines&q=&rows=20"
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records.map((el) => ({
-                name: `Piscine ${el.fields.index} | ${el.fields.telephone} | ${el.fields.adresse}`,
-                coord: el.fields.geo_point_2d,
-                key: el.recordid,
-              }))
-            )
-          );
-        break;
-      case "petanque":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=boulodromes&q=&rows=100"
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records.map((el) => ({
-                name: `Boulodrome ${el.fields.index}`,
-                coord: el.fields.geo_point_2d,
-                key: el.recordid,
-              }))
-            )
-          );
-        break;
-      case "fitness":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=site-communal-dimplantation-de-fitness&q=&rows=50"
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records.map((el) => ({
-                name: `Module de fitness ${pascalCase(el.fields.site)}`,
-                coord: el.fields.geo_point_2d,
-                key: el.recordid,
-              }))
-            )
-          );
-        break;
-      case "football":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=stades&q=&rows=50"
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records
-                .filter((el) => el.fields.foot === "O")
-                .map((el) => ({
-                  name: `Terrain de football ${el.fields.index}`,
-                  coord: el.fields.geo_point_2d,
-                  key: el.recordid,
-                }))
-            )
-          );
-        break;
-      case "rugby":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=stades&q=&rows=50"
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records
-                .filter((el) => el.fields.rugby === "O")
-                .map((el) => ({
-                  name: `Terrain de rugby ${el.fields.index}`,
-                  coord: el.fields.geo_point_2d,
-                  key: el.recordid,
-                }))
-            )
-          );
-        break;
-      case "volleyball":
-      case "handball":
-      case "basketball":
-        axios
-          .get(
-            "https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=gymnases&q=&rows=50"
-          )
-          .then((response) =>
-            setSportInfos(
-              response.data.records.map((el) => ({
-                name: `Gymnase ${el.fields.index}`,
-                coord: el.fields.geo_point_2d,
-                key: el.recordid,
-              }))
-            )
-          );
-        break;
-      default:
-        setSportInfos([
-          {
-            name: "Toulouse",
-            coord: [43.604652, 1.444209],
-            key: "1",
-          },
-        ]);
-        break;
-    }
-  };
-
   useEffect(() => {
-    getLocation();
+    getInfos(sportsSelected, setSportInfos);
   }, [sportsSelected]);
 
   return (
