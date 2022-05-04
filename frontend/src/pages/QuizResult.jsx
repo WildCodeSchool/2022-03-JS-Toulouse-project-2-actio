e82b9f0db18aae1afac02c712259bf8592345e07
shifumy <ymblandin@gmail.com>
2022-05-04 10:36:01 +0200

    get the URL params and map the receive array to display the suggested sports

--- frontend/src/pages/QuizResult.jsx
+++ frontend/src/pages/QuizResult.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import SuggestedSport from "../components/SuggestedSport";
 import "./QuizResult.css";
 
 function QuizResult() {
+  const { result } = useParams();
+  const parsedSports = JSON.parse(result);
+
   return (
     <div className="quiz-result">
       <h2>
-        Les resultats sont tombés, voici les activités que nous vous proposons :
+        Les resultats sont tombés, voici les activités que nous te proposons :
       </h2>
       <div className="sport-container">
-        <NavLink to="/map/football">
-          <SuggestedSport sport="football" />
-        </NavLink>
+        {parsedSports.map((sport) => (
+          <NavLink className="navlink-sport" to={`/map/${sport}`}>
+            <SuggestedSport sport={`${sport}`} />
+          </NavLink>
+        ))}
       </div>
     </div>
   );
