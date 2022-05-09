4ce2ee85aea8986cdd5ea59162bfbefa45de7472
shifumy <ymblandin@gmail.com>
2022-05-09 15:09:36 +0200

    finish the implementation of useContext

--- frontend/src/pages/Infos.jsx
+++ frontend/src/pages/Infos.jsx
@@ -1,120 +1,118 @@
-// import React, { useState, useEffect, useContext } from "react";
-// import { useSearchParams } from "react-router-dom";
-// import axios from "axios";
-// import { MapContainer, TileLayer, Marker } from "react-leaflet";
-// import { SportContext } from "../contexts/SportContext";
-// import ChangeSportPicture from "../components/ChangeSportPicture";
-// import "./Infos.css";
+import React, { useState, useEffect, useContext } from "react";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { SportContext } from "../contexts/SportContext";
+import ChangeSportPicture from "../components/ChangeSportPicture";
+import "./Infos.css";
 
-// function Infos() {
-//   const { sportInfos } = useContext(SportContext);
-//   const [address, setAddress] = useState("");
-//   const [searchParams] = useSearchParams();
-//   // const name = searchParams.get("name");
-//   // const coord = searchParams.get("coord");
-//   // const sport = searchParams.get("sport");
-//   const locationId = searchParams.get("id");
-//   // const fav = searchParams.get("fav");
-//   // const favBool = fav === "true";
-//   let newName = "";
-//   const newCoord = coord.split(",").map((e) => parseFloat(e));
+function Infos() {
+  const { sportInfos } = useContext(SportContext);
+  const [address, setAddress] = useState("");
+  const [searchParams] = useSearchParams();
+  const locationId = searchParams.get("id");
+  const [sport] = useState(sportInfos.find((el) => el.key === locationId));
 
-//   if (name.includes("|")) {
-//     newName = name.split("|");
-//   } else {
-//     newName = [name];
-//   }
+  let newName = "";
 
-//   useEffect(() => {
-//     axios
-//       .get(
-//         `https://api-adresse.data.gouv.fr/reverse/?lon=${newCoord[1]}&lat=${newCoord[0]}`
-//       )
-//       .then((response) => {
-//         setAddress(response.data.features[0].properties.label);
-//       });
-//   }, []);
+  if (sport.name.includes("|")) {
+    newName = sport.name.split("|");
+  } else {
+    newName = [sport.name];
+  }
 
-//   const [favourite, setFavourite] = useState(favBool);
-//   // useEffect(() => {
-//   //   axios
-//   //     .get(`http://localhost:5000/api/favourite-locations/${locationId}`)
-//   //     .then(() => setFavourite(true));
-//   // }, []);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api-adresse.data.gouv.fr/reverse/?lon=${sport.coord[1]}&lat=${sport.coord[0]}`
+      )
+      .then((response) => {
+        setAddress(response.data.features[0].properties.label);
+      });
+  }, []);
 
-//   const handleFavourite = () => {
-//     if (favourite) {
-//       axios.delete(
-//         `http://localhost:5000/api/favourite-locations/${locationId}`
-//       );
-//     } else {
-//       axios.post("http://localhost:5000/api/favourite-locations", {
-//         locationId,
-//         favourite: !favourite,
-//       });
-//     }
-//     setFavourite(!favourite);
-//   };
+  const [favourite, setFavourite] = useState(false);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/favourite-locations/${locationId}`)
+      .then(() => setFavourite(true));
+  }, []);
 
-//   return (
-//     <div className="Infos">
-//       <div className="chosenSport-picture">{ChangeSportPicture(sport)}</div>
+  const handleFavourite = () => {
+    if (favourite) {
+      axios.delete(
+        `http://localhost:5000/api/favourite-locations/${locationId}`
+      );
+    } else {
+      axios.post("http://localhost:5000/api/favourite-locations", {
+        locationId,
+        favourite: !favourite,
+      });
+    }
+    setFavourite(!favourite);
+  };
 
-//       <div className="desktop-right">
-//         <div className="location-name">
-//           <h2 className="name">
-//             <strong>{newName[0]}</strong>
-//           </h2>
-//         </div>
+  return (
+    <div className="Infos">
+      <div className="chosenSport-picture">
+        {ChangeSportPicture(sport.sport)}
+      </div>
 
-//         <div className="location-infos">
-//           <div className="fas location-details fa-xs">
-//             <img
-//               src="/src/assets/location-icone.svg"
-//               alt="icone de localisation"
-//             />
-//             <p>{address}</p>
-//           </div>
+      <div className="desktop-right">
+        <div className="location-name">
+          <h2 className="name">
+            <strong>{newName[0]}</strong>
+          </h2>
+        </div>
 
-//           <div className="fas phone-details fa-xs">
-//             <img
-//               src="/src/assets/phone-icone.svg"
-//               alt="icone de téléphone"
-//               className="phone-icone"
-//             />
-//             <p>05 61 22 32 64</p>
-//           </div>
+        <div className="location-infos">
+          <div className="fas location-details fa-xs">
+            <img
+              src="/src/assets/location-icone.svg"
+              alt="icone de localisation"
+            />
+            <p>{address}</p>
+          </div>
 
-//           <div className="fas web-icone fa-xs">
-//             <img
-//               src="/src/assets/web-icone.svg"
-//               alt="icone de web"
-//               className="web-icone"
-//             />
-//             <p>point.accueil.inscriptions@mairie-toulouse.fr</p>
-//           </div>
-//         </div>
+          <div className="fas phone-details fa-xs">
+            <img
+              src="/src/assets/phone-icone.svg"
+              alt="icone de téléphone"
+              className="phone-icone"
+            />
+            <p>05 61 22 32 64</p>
+          </div>
 
-//         <div className="map">
-//           <MapContainer center={newCoord} zoom={15}>
-//             <TileLayer
-//               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//               className="map-tiles"
-//             />
-//             <button
-//               className="btn-favourite"
-//               onClick={handleFavourite}
-//               type="submit"
-//             >
-//               {favourite ? "♥" : "♡"}
-//             </button>
-//             <Marker position={newCoord} />
-//           </MapContainer>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+          <div className="fas web-icone fa-xs">
+            <img
+              src="/src/assets/web-icone.svg"
+              alt="icone de web"
+              className="web-icone"
+            />
+            <p>point.accueil.inscriptions@mairie-toulouse.fr</p>
+          </div>
+        </div>
 
-// export default Infos;
+        <div className="map">
+          <MapContainer center={sport.coord} zoom={15}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              className="map-tiles"
+            />
+            <button
+              className="btn-favourite"
+              onClick={handleFavourite}
+              type="submit"
+            >
+              {favourite ? "♥" : "♡"}
+            </button>
+            <Marker position={sport.coord} />
+          </MapContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Infos;
